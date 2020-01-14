cb513a6d281b30ec255361e28b13438a175c8cda
rpalmer23 <59582695+rpalmer23@users.noreply.github.com>
2020-01-13 17:30:16 -0700

    Added plaindrome porogra?

--- programs/palindrome/main.cpp
+++ programs/palindrome/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string input;
+    cout << "Enter a word to see if it's a palindrome: ";
+    cin >> input;
+    int len = input.length();
+    char charArray[len];
+    char dummyArray[len];
+    strcpy(charArray, input.c_str());
+
+    int dummyCount = 0;
+    for(int i = len-1; i >= 0; i--) {
+        dummyArray[dummyCount] = charArray[i];
+        dummyCount++;
+    }
+cout << dummyArray;
+    if(strcmp(charArray,dummyArray) == 0) {
+        cout << "\nThat word is a palindrome!";
+    } else {
+        cout << "\nNot a palindrome.";
+    }
+
     return 0;
 }
