1d964d7a138ddc2304213c5bc9d8b0576a6c06fa
rpalmer23 <59582695+rpalmer23@users.noreply.github.com>
2020-01-15 14:30:19 -0700

    primenumber

--- programs/primeNumber/main.cpp
+++ programs/primeNumber/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int input;
+    int factors = 0;
+    cout << "Enter a positive integer to see if it's a prime number: ";
+    cin >> input;
+
+    for (int i = 1; i <= input; i++){
+        if (input % i == 0) {
+            factors++;
+        }
+    }
+
+    if (factors > 2) {
+        cout << input << " is not a prime number";
+    } else {
+        cout << input << " is a prime number";
+    }
     return 0;
 }
