e4e86e3eada4356d0f72b83b1db741d47c5aae8c
rpalmer23 <59582695+rpalmer23@users.noreply.github.com>
2020-01-15 14:15:54 -0700

    primenumber

---
+++ programs/primeNumber/cmake-build-debug/CMakeFiles/primeNumber.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/primeNumber.dir/main.cpp.o"
+  "primeNumber"
+  "primeNumber.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/primeNumber.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
